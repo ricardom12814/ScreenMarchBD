ca38e35cf8dabb294bfb4f5f0bffb22181f6d183
Ricardo <ricardom12814@gmail.com>
2024-06-21 18:43:04 -0500

    adicionando elementos en la carpeta screenmatch al respositorio remoto - ajustando info secreta chatGPT 3

--- src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -1,6 +1,6 @@
 package com.aluracursos.screenmatch.model;
 
-import com.aluracursos.screenmatch.service.ConsultaChatGPT;
+//import com.aluracursos.screenmatch.service.ConsultaChatGPT;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.persistence.*;
 
--- src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -6,7 +6,7 @@ import com.theokanning.openai.service.OpenAiService;
 /*
 public class ConsultaChatGPT {
     public static String obtenerTraduccion(String texto){
-        OpenAiService service = new OpenAiService("sk-proj-crtozOLG1YcGumzMmTaxT3BlbkFJW2Xk11D7caWP7CU6RVhK");
+        OpenAiService service = new OpenAiService("API KEY CHATGPT");
 
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")

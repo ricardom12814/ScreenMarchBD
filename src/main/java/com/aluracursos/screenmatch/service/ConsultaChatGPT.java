401f4a4746b3f12f3f993eaf5deb50a100148a90
Ricardo <ricardom12814@gmail.com>
2024-06-21 22:46:16 -0500

    segundo commit adicionando modificacion ajustando extension chatGPT

--- src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -1,9 +1,9 @@
 package com.aluracursos.screenmatch.service;
-
+/*
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
 
-/*
+
 public class ConsultaChatGPT {
     public static String obtenerTraduccion(String texto){
         OpenAiService service = new OpenAiService("API KEY CHATGPT");
